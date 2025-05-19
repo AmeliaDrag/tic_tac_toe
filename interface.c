fbd8f7eac06520b6405b68038aecbf527747ee80
AmeliaDrag <draganescuamelia@yahoo.com>
2025-05-19 18:08:37 +0300

    updated files

--- interface.c
+++ interface.c
@@ -55,30 +55,11 @@ void drawGameScreen() {
         if (i < 2)
             mvprintw(i * 2 + 1, 0, "---+---+---");
     }
-
+    //A_REVERSE - functie care face caracterele albe pe fundal negru.
     mvprintw(8, 0, "Scor - X: %d  O: %d  Remiza: %d", scoreX, scoreO, draws);
     refresh();
 }
-/*void drawGameScreen() {
-  for (int i = 0; i < 3; i++) { // pt fircare  rand
-        for (int j = 0; j < 3; j++) {       // pt fircare coloană
-            if (i == randulSelectat && j == coloanaSelectata) 
-                attron(A_REVERSE);          // evidențiază celula curentă
 
-            mvprintw(i * 2, j * 4, " %c ", board[i][j]); // desenează X, O sau gol. i*2 ca sa fie mai aiesitista toata treaba, sa nu fie randurile asa apropiate
-            
-            if (i == randulSelectat && j == coloanaSelectata)
-	      attroff(A_REVERSE);   //opreste 
-
-            if (j < 2) 
-                mvprintw(i * 2, j * 4 + 3, "|"); // desenăm separatorul de coloane. +3 ca separatorul sa nu fie fix langa x sau o. dar nu 4 sa nu fie chiar la final.
-        }
-        if (i < 2) 
-            mvprintw(i * 2 + 1, 0, "---+---+---"); // separator de rânduri
-    }
-  mvprintw(8,0, "Scor-X: %d O: %d, Remiza: %d", scoreX,scoreO, draws);
-  refresh();  // actualizează ecranul. standard la bibloteca ncurses.
-  }*/
 // ch e caracaterul pus. key_up e sageata in sus s tot asa
 void handleInput(int ch, int mode) {
   if (ch == KEY_UP && randulSelectat > 0) randulSelectat--;
--- main.c
+++ main.c
@@ -6,26 +6,26 @@
 #include "ai.h"
 
 //#include<stdbool>
-extern bool gameOver;
+extern bool gameOver; //accesam o functie deja existenta, nu o mai declaram odata pentru ca exista intr un alt fisier. extern spune ca deja exista intr un alt loc
 extern char currentPlayer;
 
 int main() {
   int mode = 0; //1=PVSP SI 2=PVSAI
 
-  // alegerea modului de joc la început
+  // alegerea modului de joc la început, asta e practic doar afisarea meniului nu si implementarea sa, implementarea e in interface.c
   printf("Select game mode:\n");
   printf("1. Player vs Player\n");
   printf("2. Player vs AI\n");
   printf("Enter choice: ");
-  scanf("%d", &mode);
+  scanf("%d", &mode); //citeste modul introdus
 
   initInterface();   // Initializeaza ncurses
   loadScores();      // Încarcă scorurile din fișier
 
-  int running = 1;
+  int running = 1; //valoarea 1 inseamna da, adica ruleaza jocul
   while (running) {
     initGame();        // InitializeazA tabla de joc
-    gameOver = false;
+    gameOver = false; //daca un singur meci s a incheiata
 
     // bucla principală pentru un meci
     while (!gameOver) { // Cat timp jocul nu s a terminat
@@ -43,11 +43,11 @@ int main() {
           mvprintw(8, 0, "Jucatorul %c a castigat!\n", winner);
         }
 	else {
-          mvprintw(8, 0, "Egalitate!\n");
+          mvprintw(8, 0, "Egalitate!\n"); //mvprintw este una din functiile ncurses care se refera la move cursor print window
         }
 
-        refresh();
-        updateScore(winner == ' ' ? 'D' : winner); // update scor; D pt remiză
+        refresh(); // functie ncurses care actualizeaza ecranul cu toate modificarile pe care le ai facut in memorie
+        updateScore(winner == ' ' ? 'D' : winner); // update scor; D de la draw pt remiză
         saveScores();
         mvprintw(10, 0, "Apasa enter  pentru a continua...");
         getch();  // Asteapta apasarea unei taste
@@ -55,19 +55,19 @@ int main() {
       }
 
       // verificare remiză (dacă nu mai sunt celule libere)
-      int filled = 1;
-      for (int i = 0; i < 3 && filled; i++) {
+      int filled = 1; // presupunem ca tabla e umpluta 
+      for (int i = 0; i < 3 && filled; i++) { // parcurgem tabla, pentru conditia && filled inseamna ca daca gaseste deja un spatiu gol, numai are rost sa cautam mai departe
         for (int j = 0; j < 3; j++) {
           if (board[i][j] == ' ') {
-            filled = 0;
+            filled = 0; //tabla nu este complet goala si iesim din buvla prin break
             break;
           }
         }
       }
-      if (filled) {
-        clear();
+      if (filled) { //daca e umpluta 
+        clear(); //functie specifica in ncurses
         drawGameScreen();
-        mvprintw(8, 0, "Egalitate!");
+        mvprintw(8, 0, "Egalitate!"); // este in if de la checkWinner gen daca tabla e plina, dar nu a castigat nimeni
         refresh();
         updateScore('D');
         saveScores();
@@ -78,12 +78,12 @@ int main() {
     }
 
     // întrebăm dacă vrei să joci din nou
-    clear();
+    clear(); // curata tot. stergi tot pentru a reincepe desenarea de la 0.
     mvprintw(5, 0, "Vrei sa joci din nou? (y/n): ");
-    refresh();
+    refresh(); //aplici efectiv ce ai scris in mvprintw.
     int answer = getch();
     if (answer != 'y' && answer != 'Y') {
-      running = 0;
+      running = 0; // se opreste
     }
   }
 
